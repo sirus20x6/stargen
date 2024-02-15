f8b35ca91e90084f0a5e527e72db8949a2e482b6
sirus20x6 <sirus20x6@gmail.com>
2024-02-15 15:05:26 -0600

    continued converting consts.h to constexpr. pretty much done. comments are a mess. will re-do comments later.

--- const.h
+++ const.h
@@ -68,30 +68,34 @@ constexpr double GAS_RETENTION_THRESHOLD = 6.0; /* ratio of esc vel to RMS vel *
 constexpr double ICE_ALBEDO = 0.7;
 constexpr double CLOUD_ALBEDO = 0.52;
 constexpr double GAS_GIANT_ALBEDO = 0.5; /* albedo of a gas giant	*/
-#define CLASS_I_ALBEDO (0.57)
-#define CLASS_II_ALBEDO (0.81)
-#define CLASS_III_ALBEDO (0.12)
-#define CLASS_IV_ALBEDO (0.03)
-#define CLASS_V_ALBEDO (0.55)
-#define CARBON_GIANT_ALBEDO (0.01)
-#define SULFAR_GIANT_ALBEDO (0.63)
-#define METHANE_GIANT_ALBEDO (0.3)
-#define AIRLESS_ICE_ALBEDO (0.5)
-#define EARTH_ALBEDO (0.3) /* was .33 for a while */
-#define GREENHOUSE_TRIGGER_ALBEDO (0.20)
-#define ROCKY_ALBEDO (0.15)
-#define ROCKY_AIRLESS_ALBEDO (0.07)
-#define WATER_ALBEDO (0.04)
-#define TEMPERATURE_NEPTUNE (48.1)
-#define TEMPERATURE_URANUS (60.3)
-#define TEMPERATURE_CLASS_I (81.0)
-#define TEMPERATURE_SATURN (85.1)
-#define TEMPERATURE_CLASS_II (150.0)
-#define TEMPERATURE_SULFUR_GIANT (320.0)
-#define TEMPERATURE_CLASS_III (360.0)
-#define TEMPERATURE_CLASS_IV (900.0)
-#define TEMPERATURE_CLASS_V (1400.0)
-#define TEMPERATURE_CARBON_GIANT (2240.0)
+
+// Albedo values for various celestial body classifications
+constexpr double CLASS_I_ALBEDO = 0.57;
+constexpr double CLASS_II_ALBEDO = 0.81;
+constexpr double CLASS_III_ALBEDO = 0.12;
+constexpr double CLASS_IV_ALBEDO = 0.03;
+constexpr double CLASS_V_ALBEDO = 0.55;
+constexpr double CARBON_GIANT_ALBEDO = 0.01;
+constexpr double SULFAR_GIANT_ALBEDO = 0.63;
+constexpr double METHANE_GIANT_ALBEDO = 0.3;
+constexpr double AIRLESS_ICE_ALBEDO = 0.5;
+constexpr double EARTH_ALBEDO = 0.3;
+constexpr double GREENHOUSE_TRIGGER_ALBEDO = 0.20;
+constexpr double ROCKY_ALBEDO = 0.15;
+constexpr double ROCKY_AIRLESS_ALBEDO = 0.07;
+constexpr double WATER_ALBEDO = 0.04;
+
+// Temperature constants for various celestial bodies
+constexpr double TEMPERATURE_NEPTUNE = 48.1;
+constexpr double TEMPERATURE_URANUS = 60.3;
+constexpr double TEMPERATURE_CLASS_I = 81.0;
+constexpr double TEMPERATURE_SATURN = 85.1;
+constexpr double TEMPERATURE_CLASS_II = 150.0;
+constexpr double TEMPERATURE_SULFUR_GIANT = 320.0;
+constexpr double TEMPERATURE_CLASS_III = 360.0;
+constexpr double TEMPERATURE_CLASS_IV = 900.0;
+constexpr double TEMPERATURE_CLASS_V = 1400.0;
+constexpr double TEMPERATURE_CARBON_GIANT = 2240.0;
 
 constexpr double SECONDS_PER_HOUR = 3600.0;
 constexpr double CM_PER_AU = 1.495978707E13;
@@ -118,184 +122,219 @@ constexpr double INCREDIBLY_LARGE_NUMBER = 9.9999E37;
 /*	Now for a few molecular weights (used for RMS velocity calcs):	   */
 /*	This table is from Dole's book "Habitable Planets for Man", p. 38  */
 
+// Atomic and molecular weights
 constexpr double ATOMIC_HYDROGEN = 1.0;
-#define MOL_HYDROGEN (2.0)       /* H2  */
-#define HELIUM (4.0)             /* He  */
-#define ATOMIC_NITROGEN (14.0)   /* N   */
-#define ATOMIC_OXYGEN (16.0)     /* O   */
-#define METHANE (16.0)           /* CH4 */
-#define AMMONIA (17.0)           /* NH3 */
-#define WATER_VAPOR (18.0)       /* H2O */
-#define NEON (20.2)              /* Ne  */
-#define MOL_NITROGEN (28.0)      /* N2  */
-#define CARBON_MONOXIDE (28.0)   /* CO  */
-#define NITRIC_OXIDE (30.0)      /* NO  */
-#define MOL_OXYGEN (32.0)        /* O2  */
-#define HYDROGEN_SULPHIDE (34.1) /* H2S */
-#define ARGON (39.9)             /* Ar  */
-#define CARBON_DIOXIDE (44.0)    /* CO2 */
-#define NITROUS_OXIDE (44.0)     /* N2O */
-#define NITROGEN_DIOXIDE (46.0)  /* NO2 */
-#define OZONE (48.0)             /* O3  */
-#define SULPH_DIOXIDE (64.1)     /* SO2 */
-#define SULPH_TRIOXIDE (80.1)    /* SO3 */
-#define KRYPTON (83.8)           /* Kr  */
-#define XENON (131.3)            /* Xe  */
+constexpr double MOL_HYDROGEN = 2.0;
+constexpr double HELIUM = 4.0;
+constexpr double ATOMIC_NITROGEN = 14.0;
+constexpr double ATOMIC_OXYGEN = 16.0;
+constexpr double METHANE = 16.0;
+constexpr double AMMONIA = 17.0;
+constexpr double WATER_VAPOR = 18.0;
+constexpr double NEON = 20.2;
+constexpr double MOL_NITROGEN = 28.0;
+constexpr double CARBON_MONOXIDE = 28.0;
+constexpr double NITRIC_OXIDE = 30.0;
+constexpr double MOL_OXYGEN = 32.0;
+constexpr double HYDROGEN_SULPHIDE = 34.1;
+constexpr double ARGON = 39.9;
+constexpr double CARBON_DIOXIDE = 44.0;
+constexpr double NITROUS_OXIDE = 44.0;
+constexpr double NITROGEN_DIOXIDE = 46.0;
+constexpr double OZONE = 48.0;
+constexpr double SULPH_DIOXIDE = 64.1;
+constexpr double SULPH_TRIOXIDE = 80.1;
+constexpr double KRYPTON = 83.8;
+constexpr double XENON = 131.3;
 
 //	And atomic numbers, for use in ChemTable indexes
-#define AN_H 1
-#define AN_HE 2
-#define AN_N 7
-#define AN_O 8
-#define AN_F 9
-#define AN_NE 10
-#define AN_P 15
-#define AN_CL 17
-#define AN_AR 18
-#define AN_BR 35
-#define AN_KR 36
-#define AN_I 53
-#define AN_XE 54
-#define AN_HG 80
-#define AN_AT 85
-#define AN_RN 86
-#define AN_FR 87
+constexpr int AN_H = 1;
+constexpr int AN_HE = 2;
+constexpr int AN_N = 7;
+constexpr int AN_O = 8;
+constexpr int AN_F = 9;
+constexpr int AN_NE = 10;
+constexpr int AN_P = 15;
+constexpr int AN_CL = 17;
+constexpr int AN_AR = 18;
+constexpr int AN_BR = 35;
+constexpr int AN_KR = 36;
+constexpr int AN_I = 53;
+constexpr int AN_XE = 54;
+constexpr int AN_HG = 80;
+constexpr int AN_AT = 85;
+constexpr int AN_RN = 86;
+constexpr int AN_FR = 87;
 
-#define AN_NH3 900
-#define AN_H2O 901
-#define AN_CO2 902
-#define AN_O3 903
-#define AN_CH4 904
-#define AN_CH3CH2OH 905
+constexpr int AN_NH3 = 900;
+constexpr int AN_H2O = 901;
+constexpr int AN_CO2 = 902;
+constexpr int AN_O3 = 903;
+constexpr int AN_CH4 = 904;
+constexpr int AN_CH3CH2OH = 905;
 
 /*	The following defines are used in the kothari_radius function in
  */
 /*	file enviro.c.
  */
-#define A1_20 (6.485E12)  /* All units are in cgs system.	 */
-#define A2_20 (4.0032E-8) /*	 ie: cm, g, dynes, etc.		 */
-#define BETA_20 (5.71E12)
+ // Additional constants for calculations
+constexpr double A1_20 = 6.485E12;  /* All units are in cgs system.	 */
+constexpr double A2_20 = 4.0032E-8; /*	 ie: cm, g, dynes, etc.		 */
+constexpr double BETA_20 = 5.71E12;
 
-#define JIMS_FUDGE (1.004)
+constexpr double JIMS_FUDGE = 1.004;
 
-#define SUNMAG 4.83         /* absolute magnitude of the sun */
-#define N2 2.51188643150958 /* 5th root of 100 */
+constexpr double SUNMAG = 4.83;         /* absolute magnitude of the sun */
+constexpr double N2 = 2.51188643150958; /* 5th root of 100 */
 
 /*	 The following defines are used in determining the fraction of a planet
  */
 /*	covered with clouds in function cloud_fraction in file enviro.c.
  */
-#define Q1_36 (1.258E19) /* grams	*/
-#define Q2_36 (0.0698)   /* 1/Kelvin */
+constexpr double Q1_36 = 1.258E19; /* grams	*/
+constexpr double Q2_36 = 0.0698;   /* 1/Kelvin */
 
-#define PARSEC 3.2615638 /*light years*/
+constexpr double PARSEC = 3.2615638; /*light years*/
 
-#define AVOGADRO 6.02214179E23
-#define H 6.62606E-34                            /*Planck constant*/
-#define C 299792458                              /*Speed of light*/
-#define KB (MOLAR_GAS_CONST / 1000.0) / AVOGADRO /*Boltzmann constant*/
-#define ACCURACY_FOR_PEAK 0.01
+constexpr double AVOGADRO = 6.02214179E23;
+constexpr double H = 6.62606E-34;    /*Planck constant*/
+constexpr double C = 299792458;     /*Speed of light*/
+constexpr double KB = (MOLAR_GAS_CONST / 1000.0) / AVOGADRO; /*Boltzmann constant*/
+constexpr double ACCURACY_FOR_PEAK = 0.01;
 
-/* macros: */
-#define pow2(a) ((a) * (a))
-#define pow3(a) ((a) * (a) * (a))
-#define pow4(a) ((a) * (a) * (a) * (a))
-#define pow1_4(a) std::sqrt(std::sqrt(a))
-#define pow1_3(a) std::pow(a, (1.0 / 3.0))
-#define pow1_2(a) std::sqrt(a)
-/*define mass(a) std::pow(a, (1.0/3.5)) calculates mass based on luminosity (a) */
-// define mass(a) (a <= (0.3815*pow(0.6224,2.5185))) ?
-// ((log(a)/log(2.5185))/0.6224) : (a <= 1) ? (log(a)/log(4.551)) : (a <=
-// std::pow(3.1623, 4.351)) ? (log(a)/log(4.351)) : (a <= (2.7563*pow(16,3.4704))) ?
-// ((log(a)/log(3.4704))/2.7563) : ((log(a)/log(2.4853))/42.321) /* calculates
+
+constexpr double pow2(double a) {
+    return a * a;
+}
+
+constexpr double pow3(double a) {
+    return a * a * a;
+}
+
+constexpr double pow4(double a) {
+    return a * a * a * a;
+}
+
+// Note: std::sqrt and std::pow are not constexpr until C++20, so these functions
+// cannot be used in a constexpr context in earlier standards.
+constexpr double pow1_4(double a) {
+    return std::sqrt(std::sqrt(a));
+}
+
+constexpr double pow1_3(double a) {
+    return std::pow(a, 1.0 / 3.0);
+}
+
+constexpr double pow1_2(double a) {
+    return std::sqrt(a);
+}
 // mass based on luminosity (a) */
-#define mass(a)                                                             \
-  (a <= (0.3815 * std::pow(0.6224, 2.5185)))                                     \
-      ? (1.46613 * std::pow(a, 0.3970617431010522))                              \
-  : (a <= 1)                  ? std::pow(a, 0.2197319270490002)                  \
-  : (a <= std::pow(3.1623, 4.351)) ? std::pow(a, 0.2298322224775914)                  \
-  : (a <= (2.7563 * std::pow(16, 3.4704)))                                       \
-      ? (0.746654 * std::pow(a, 0.2881512217611803))                             \
-      : (0.221579 * std::pow(a, 0.4023659115599726)) /* calculates mass based on \
-                                                   luminosity (a) */
+constexpr double mass(double a) {
+    if (a <= (0.3815 * std::pow(0.6224, 2.5185))) {
+        return 1.46613 * std::pow(a, 0.3970617431010522);
+    } else if (a <= 1) {
+        return std::pow(a, 0.2197319270490002);
+    } else if (a <= std::pow(3.1623, 4.351)) {
+        return std::pow(a, 0.2298322224775914);
+    } else if (a <= (2.7563 * std::pow(16, 3.4704))) {
+        return 0.746654 * std::pow(a, 0.2881512217611803);
+    } else {
+        return 0.221579 * std::pow(a, 0.4023659115599726);
+    }
+}
 
 /* calculates luminosity based on absolute magnitude (a) */
-#define abs2luminosity(a) (std::pow(N2, (SUNMAG - a)))
+constexpr double abs2luminosity(double a) { return std::pow(N2, (SUNMAG - a)); }
+constexpr double vis2abs(double v, double d) { return v - 5 * (std::log10(d / PARSEC) - 1); }
 
-#define vis2abs(v, d) (v - 5 * (log10(d / PARSEC) - 1))
+constexpr double JUPITER_MASS = 317.8; /* mass of Jupiter in Earth Masses */
+constexpr double EM(double x) {
+    return x / SUN_MASS_IN_EARTH_MASSES;
+}
 
-#define EM(x) (x) / SUN_MASS_IN_EARTH_MASSES
-#define AVE(x, y) ((x + y) / 2.0)
-#define ADD(x, y) (x + y)
-#define DIVIDE(x, y) (x / y)
+constexpr double AVE(double x, double y) {
+    return (x + y) / 2.0;
+}
 
-#define JUPITER_CORE_RATIO(x) (EM((x / JUPITER_MASS) * 10.0))
-#define JUPITER_GAS_RATIO(x) (EM(x) - JUPITER_CORE_RATIO(x))
+constexpr double ADD(double x, double y) {
+    return x + y;
+}
 
-#define JUPITER_MASS 317.8        /* mass of Jupiter in Earth Masses */
-#define KM_JUPITER_RADIUS 69911.0 /* average radius of Jupiter in km */
+constexpr double DIVIDE(double x, double y) {
+    return x / y;
+}
 
-#define ROCK_DENSITY                                                      \
-  4.7825 /* Units of g/cc (derived from ((EARTH_DENSITY - (IRON_DENSITY / \
-            3))/(2/3)) */
-#define CARBON_DENSITY \
-  3.22 /* Units of g/cc (this is actually the density of SiC) */
+constexpr double JUPITER_CORE_RATIO(double x) {
+    return EM((x / JUPITER_MASS) * 10.0);
+}
 
-#define GRAPHITE_DENSITY 2.25 /* Units of g/cc */
-#define IRON_DENSITY 7.874    /* Units of g/cc */
-#define FeS_DENSITY 4.77      /* Units of g/cc */
-#define ICE_DENSITY \
-  1.46 /* Units of g/cc (this is actually the density of ice VII) */
-#define MgO_DENSITY 3.56    /* Units of g/cc */
-#define MgSiO3_DENSITY 4.10 /* Units of g/cc */
-#define SiC_DENSITY 3.22    /* Units of g/cc */
+constexpr double JUPITER_GAS_RATIO(double x) {
+    return EM(x) - JUPITER_CORE_RATIO(x);
+}
 
-#define K2                                                                     \
-  1.7E+16 /* Years. Value from "Regarding the Criteria for Planethood And      \
-             Proposed Planetary Classification Schemes by S. Allan Stern and   \
-             Harold F. Levison. Used to determine what is a planet and what is \
-             a dwarf planet */
+constexpr double KM_JUPITER_RADIUS = 69911.0; /* average radius of Jupiter in km */
+// Units of g/cc (derived from ((EARTH_DENSITY - (IRON_DENSITY / 3))/(2/3)))
+constexpr double ROCK_DENSITY = 4.7825;
+constexpr double SiC_DENSITY = 3.22;
+constexpr double CARBON_DENSITY = SiC_DENSITY;
+constexpr double GRAPHITE_DENSITY = 2.25;
+constexpr double IRON_DENSITY = 7.874;
+constexpr double FeS_DENSITY = 4.77;
+// Units of g/cc (this is actually the density of ice VII)
+constexpr double ICE_DENSITY = 1.46;
+constexpr double MgO_DENSITY = 3.56;
+constexpr double MgSiO3_DENSITY = 4.10;
 
-#define RECENT_VENUS 1
-#define RUNAWAY_GREENHOUSE 2
-#define MOIST_GREENHOUSE 3
-#define MAXIMUM_GREENHOUSE 4
-#define EARLY_MARS 5
-#define TWO_AU_CLOUD_LIMIT 6
-#define FIRST_CO2_CONDENSATION_LIMIT 7
-#define EARTH_LIKE 8
 
-#define NONE 0
-#define BREATHABLE 1
-#define UNBREATHABLE 2
-#define POISONOUS 3
+/* Years. Value from "Regarding the Criteria for Planethood And
+   Proposed Planetary Classification Schemes by S. Allan Stern and
+   Harold F. Levison. Used to determine what is a planet and what is
+   a dwarf planet */
+constexpr double K2 = 1.7E+16;
+
+
+// Climate classification constants
+constexpr int RECENT_VENUS = 1;
+constexpr int RUNAWAY_GREENHOUSE = 2;
+constexpr int MOIST_GREENHOUSE = 3;
+constexpr int MAXIMUM_GREENHOUSE = 4;
+constexpr int EARLY_MARS = 5;
+constexpr int TWO_AU_CLOUD_LIMIT = 6;
+constexpr int FIRST_CO2_CONDENSATION_LIMIT = 7;
+constexpr int EARTH_LIKE = 8;
+
+// Atmosphere composition constants
+constexpr int NONE = 0;
+constexpr int BREATHABLE = 1;
+constexpr int UNBREATHABLE = 2;
+constexpr int POISONOUS = 3;
+
 
 // Define the color scheme. Black, Brown and Beige (with a nod to the Duke)
 
 // Main page colors: Beige BG, Dark brown text, Red links
-#define BGCOLOR "#FFCC99"
-#define TXCOLOR "#330000"
-#define LINKCOLOR "#990000"
-#define ALINKCOLOR "#FF0000"
+constexpr const char* BGCOLOR = "#FFCC99";
+constexpr const char* TXCOLOR = "#330000";
+constexpr const char* LINKCOLOR = "#990000";
+constexpr const char* ALINKCOLOR = "#FF0000";
 
 // Contrasting headers: Light brown with black text
-#define BGHEADER "#CC9966"
-#define TXHEADER "#000000"
+constexpr const char* BGHEADER = "#CC9966";
+constexpr const char* TXHEADER = "#000000";
 
 // Space, background for planets, black with sand colored letters
-#define BGSPACE "#000000"
-#define TXSPACE "#FFE6CC"
+constexpr const char* BGSPACE = "#000000";
+constexpr const char* TXSPACE = "#FFE6CC";
 
 // Main table color scheme: Sand with black (space reversed)
-#define BGTABLE "#FFE6CC"
-#define TXTABLE "#000000"
+constexpr const char* BGTABLE = "#FFE6CC";
+constexpr const char* TXTABLE = "#000000";
 
 // Notices: Post-It yellow with normal text
+constexpr const char* BGNOTE = "#FFFF66";
+constexpr const char* TXNOTE = TXCOLOR; // Reuses TXCOLOR for text color in notes
 
-#define BGNOTE "#FFFF66"
-#define TXNOTE TXCOLOR
-
-#define STARGEN_URL \
-  "http://www.eldacur.com/~brons/NerdCorner/StarGen/StarGen.html"
+// URL for StarGen's main page
+constexpr const char* STARGEN_URL = "http://www.eldacur.com/~brons/NerdCorner/StarGen/StarGen.html";
 
 #endif
